db2491d4a5cb3eeaf8ba18b84a882165dfd482fe
khang <zasadg321@gmail.com>
2020-10-04 10:48:26 +0700

    body and carousel update

--- development/index.js
+++ development/index.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62666" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64355" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
