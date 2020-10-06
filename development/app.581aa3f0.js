ad3c3c1108716fb65920d5f24b923c230ae55c64
khang <zasadg321@gmail.com>
2020-10-06 18:25:14 +0700

    update first page

--- development/app.581aa3f0.js
+++ development/app.581aa3f0.js
@@ -195,32 +195,55 @@ var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
 },{"./..\\images\\SmilingStudent.jpg":[["SmilingStudent.e005ff72.jpg","../src/images/SmilingStudent.jpg"],"../src/images/SmilingStudent.jpg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../src/js/main.js":[function(require,module,exports) {
-$(document).ready(function () {
-  // executes when HTML-Document is loaded and DOM is ready
+// $(document).ready(function() {
+// // //     // executes when HTML-Document is loaded and DOM is ready
+// // //     /*
+// // //     ################
+// // //     Add navbar background color when scrolled
+// // //     */
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 56) {
+    $(".navbar").addClass("bg-white");
+  } else {
+    $(".navbar").removeClass("bg-white");
+  }
+}); // //     // If Mobile, add background color when toggler is clicked
+// $(".navbar-toggler").click(function() {
+//   if (!$(".navbar-collapse").hasClass("show")) {
+//     $(".navbar").addClass("bg-white");
+//   } 
+//   else {
+//     if ($(window).scrollTop() < 56) {
+//       $(".navbar").removeClass("bg-white");
+//     } 
+// //     else {
+// //     }
+//   }
+// });
+// //     // document ready
+//   });
 
-  /*
-  ################
-  Add navbar background color when scrolled
-  */
-  $(window).scroll(function () {
-    if ($(window).scrollTop() > 56) {
-      $(".navbar").addClass("bg-white");
-    } else {
+var navbar = document.querySelector('nav');
+var navbarCollapse = document.querySelector('navbar-collapse');
+var navbarToggler = document.getElementById('burger');
+
+window.onscroll = function () {
+  if (window.scrollY > 50) {
+    navbar.classList.add('bg-white');
+  } else {
+    navbar.classList.remove('bg-white');
+  }
+};
+
+navbarToggler.onclick = function () {
+  if (!$(".navbar-collapse").hasClass("show")) {
+    $(".navbar").addClass("bg-white");
+  } else {
+    if ($(window).scrollTop() < 50) {
       $(".navbar").removeClass("bg-white");
     }
-  }); // If Mobile, add background color when toggler is clicked
-
-  $(".navbar-toggler").click(function () {
-    if (!$(".navbar-collapse").hasClass("show")) {
-      $(".navbar").addClass("bg-white");
-    } else {
-      if ($(window).scrollTop() < 56) {
-        $(".navbar").removeClass("bg-white");
-      } else {}
-    }
-  }); // ############
-  // document ready
-});
+  }
+};
 },{}],"../src/app.js":[function(require,module,exports) {
 "use strict";
 
@@ -257,7 +280,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62201" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51575" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,9 @@
-$(document).ready(function() {
-    // executes when HTML-Document is loaded and DOM is ready
-  
-  
-    /*
-    ################
-    Add navbar background color when scrolled
-    */
+// $(document).ready(function() {
+// // //     // executes when HTML-Document is loaded and DOM is ready
+// // //     /*
+// // //     ################
+// // //     Add navbar background color when scrolled
+// // //     */
     $(window).scroll(function() {
       if ($(window).scrollTop() > 56) {
         $(".navbar").addClass("bg-white");
@@ -13,18 +11,55 @@ $(document).ready(function() {
         $(".navbar").removeClass("bg-white");
       }
     });
-    // If Mobile, add background color when toggler is clicked
-    $(".navbar-toggler").click(function() {
-      if (!$(".navbar-collapse").hasClass("show")) {
-        $(".navbar").addClass("bg-white");
-      } else {
-        if ($(window).scrollTop() < 56) {
-          $(".navbar").removeClass("bg-white");
-        } else {
-        }
-      }
-    });
-    // ############
+// //     // If Mobile, add background color when toggler is clicked
+    // $(".navbar-toggler").click(function() {
+    //   if (!$(".navbar-collapse").hasClass("show")) {
+    //     $(".navbar").addClass("bg-white");
+    //   } 
+    //   else {
+    //     if ($(window).scrollTop() < 56) {
+    //       $(".navbar").removeClass("bg-white");
+    //     } 
+    // //     else {
+    // //     }
+    //   }
+    // });
+   
+// //     // document ready
+//   });
+
+ 
+let navbar = document.querySelector('nav');
+let navbarCollapse = document.querySelector('navbar-collapse');
+let navbarToggler = document.getElementById('burger');
+
+window.onscroll = function () {
+  if (window.scrollY > 50){
+    navbar.classList.add('bg-white');
+  }
+  else{
+    navbar.classList.remove('bg-white');
+  }
+}
+
+navbarToggler.onclick= function(){
   
-    // document ready
-  });
+    if (!$(".navbar-collapse").hasClass("show")) {
+      $(".navbar").addClass("bg-white");
+    } 
+    else {
+      if ($(window).scrollTop() < 50) {
+        $(".navbar").removeClass("bg-white");
+      } 
+    }
+  }
+ 
+
+    
+
+
+   
+
+
+    
+
