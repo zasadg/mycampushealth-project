e8909f438f190e9ac15252fe4d79e6005545b90c
khang <zasadg321@gmail.com>
2020-10-11 00:36:08 +0700

    update modal and footer

--- src/js/style2.js
+++ src/js/style2.js
@@ -30,10 +30,70 @@ $(document).ready(function() {
     $('#vModal-3').on('hide.bs.modal', function (e) {
         $("#video-3").attr('src',$videoSrc); 
     }) 
+
+    $('#vModal-4').on('shown.bs.modal', function (e) {
+        $("#video-4").attr('src',$videoSrc + "?rel=0&showinfo=0&modestbranding=1&autoplay=1" ); 
+        })
+      
+        $('#vModal-4').on('hide.bs.modal', function (e) {
+            $("#video-4").attr('src',$videoSrc); 
+        }) 
+    
+    $('#vModal-4').on('shown.bs.modal', function (e) {
+        $("#video-5").attr('src',$videoSrc + "?rel=0&showinfo=0&modestbranding=1&autoplay=1" ); 
+        })
+      
+        $('#vModal-5').on('hide.bs.modal', function (e) {
+            $("#video-5").attr('src',$videoSrc); 
+        }) 
+
+    $('#vModal-6').on('shown.bs.modal', function (e) {
+        $("#video-6").attr('src',$videoSrc + "?rel=0&showinfo=0&modestbranding=1&autoplay=1" ); 
+        })
+      
+        $('#vModal-6').on('hide.bs.modal', function (e) {
+            $("#video-6").attr('src',$videoSrc); 
+        }) 
   });
   
     
   
   
-  
+  $('.responsive').slick({
+    dots: true,
+      prevArrow: $('.prev'),
+      nextArrow: $('.next'),
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
   
