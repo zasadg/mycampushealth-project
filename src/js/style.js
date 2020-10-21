379e5204f12cdc78ba0e3a4ab9f130603a4bfa74
khang <zasadg321@gmail.com>
2020-10-21 19:42:47 +0700

    fix navbar and add content need to add shadow to navbar

--- src/js/style.js
+++ src/js/style.js
@@ -1,6 +1,7 @@
 let navbar = document.querySelector('nav');
 let navbarCollapse = document.querySelector('navbar-collapse');
 let navbarToggler = document.getElementById('burger');
+let shadow = document.getElementById('shadow');
 
 window.onscroll = function () {
 if (window.scrollY > 50){
@@ -12,16 +13,24 @@ else{
 }
 
 navbarToggler.onclick= function(){
-
   if (!$(".navbar-collapse").hasClass("show")) {
     $(".navbar").addClass("bg-white");
   } 
   else {
     if ($(window).scrollTop() < 50) {
-      $(".navbar").removeClass("bg-white");
+      $(".navbar").removeClass("bg-white");     
     } 
   }
 }
 
+// $(document).ready(function(){
+//   $(window).scroll(function(){
+//    var y = $(window).scrollTop();
+//    if( y > 0 ){
+//        $("#shadow").css({'display':'block', 'opacity':y/20});;
 
-
+//    } else {
+//        $("#shadow").css({'display':'none', 'opacity':y/20});
+//    }
+//   });
+//  })
