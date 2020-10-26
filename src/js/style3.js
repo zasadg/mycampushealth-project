026b650d66254055ff8148092cc5897674b19567
khang <zasadg321@gmail.com>
2020-10-26 22:41:09 +0700

    update something

--- src/js/style3.js
+++ src/js/style3.js
@@ -1,3 +1,6 @@
+
+
+
 //Cache
 var $card = $('.js-card'),
     $cardGrid = $('.js-card-grid');
@@ -159,4 +162,42 @@ $cardGrid.on('click', '.js-card', function(event) {
     }
         
  });
+
+ $('.responsive').slick({
+  dots: true,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
 
--- src/js/style4.js
+++ src/js/style4.js
@@ -1,22 +1,40 @@
-// flip Cards
-+ function($) {
-    'use strict';
-    function flipCard() {
-        $('.js-flip').click(function(e) {
-            e.preventDefault();
-            $(this).closest('.js-card').addClass('hover');
-        });
-      
-      $('.js-flipClose').click(function(e) {
-            e.preventDefault();
-            $(this).closest('.js-card').removeClass('hover');
-        });
-    }
-    flipCard();
-}(jQuery);
+jQuery(document).ready(function() {
+    
+    var text1 = jQuery('.transaction_activity_text1').height() + 4;
+    var text2 = jQuery('.transaction_activity_text2').height() + 4;
+    var text3 = jQuery('.transaction_activity_text3').height() + 4;
+    var text4 = jQuery('.transaction_activity_text4').height() + 4;
 
-var app = angular.module('flipCardApp', []);
+    jQuery('.transaction_activity_text1, .transaction_activity_image_dots1').click(function() {
+        jQuery('.transaction_activity_text_click').css({'top': '0', 'height': text1});
+        jQuery('.transaction_activity_text div').css({'color': '#2c3f5733'});
+        jQuery('.transaction_activity_text1').css({'color':'#fff'});
+        jQuery('.transaction_activity_image_dots div').css({'background-color': '#c5c7ca'});
+        jQuery('.transaction_activity_image_dots1').css({'background-color':'#31d317'});
+    });
 
-app.controller('cardCtrl',['$scope', function($scope){
-  $scope.cupsValue = 2;
-}]);
+    jQuery('.transaction_activity_text2, .transaction_activity_image_dots2').click(function() {
+        jQuery('.transaction_activity_text_click').css({'top': text1, 'height': text2});
+        jQuery('.transaction_activity_text div').css({'color': '#2c3f5733'});
+        jQuery('.transaction_activity_text2').css({'color':'#fff'});
+        jQuery('.transaction_activity_image_dots div').css({'background-color': '#c5c7ca'});
+        jQuery('.transaction_activity_image_dots2').css({'background-color':'#31d317'});
+    });
+
+    jQuery('.transaction_activity_text3, .transaction_activity_image_dots3').click(function() {
+        jQuery('.transaction_activity_text_click').css({'top': text1 + text2, 'height': text3});
+        jQuery('.transaction_activity_text div').css({'color': '#2c3f5733'});
+        jQuery('.transaction_activity_text3').css({'color':'#fff'});
+        jQuery('.transaction_activity_image_dots div').css({'background-color': '#c5c7ca'});
+        jQuery('.transaction_activity_image_dots3').css({'background-color':'#31d317'});
+    }); 
+
+    jQuery('.transaction_activity_text4, .transaction_activity_image_dots4').click(function() {
+        jQuery('.transaction_activity_text_click').css({'top': text1 + text2 + text3, 'height': text4});
+        jQuery('.transaction_activity_text div').css({'color': '#2c3f5733'});
+        jQuery('.transaction_activity_text4').css({'color':'#fff'});
+        jQuery('.transaction_activity_image_dots div').css({'background-color': '#c5c7ca'});
+        jQuery('.transaction_activity_image_dots4').css({'background-color':'#31d317'});
+    });
+
+});
