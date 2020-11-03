56a54e5fd14fd73b5171a4d680179e529127b352
khang <zasadg321@gmail.com>
2020-11-03 17:30:57 +0700

    update contactus

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,45 @@ import './style4';
 import './style';
 import './style2';
 import './style3';
+$(function() {
+    // init the validator
+    // validator files are included in the download package
 
+  
+    $("#contact-form").validator();
+  
+    // when the form is submitted
+    $("#contact-form").on("submit", function(e) {
+      // if the validator does not prevent form submit
+      if (!e.isDefaultPrevented()) {
+        var url = "contact.php";
+
+  
+        var messageAlert = "alert-success";
+        var messageText =
+          "Your message was sent, thank you. I will get back to you soon.";
+  
+        // compose Bootstrap alert box HTML
+        var alertBox =
+          '<div class="alert ' +
+          messageAlert +
+          ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
+          messageText +
+          "</div>";
+  
+        // If we have messageAlert and messageText
+        if (messageAlert && messageText) {
+          // inject the alert to .messages div in our form
+          $("#contact-form").find(".messages").html(alertBox);
+          // empty the form
+          $("#contact-form")[0].reset();
+        }
+  
+        return false;
+      }
+    });
+  });
+  
 
 
     
