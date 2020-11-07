91fdcbee32d5b520425036d3fde66baab52f39de
khang <zasadg321@gmail.com>
2020-11-07 16:12:22 +0700

    update pages

--- development/app.3525348d.js
+++ development/app.3525348d.js
@@ -122,7 +122,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./..\\images\\SmilingStudent-small.jpg":[["SmilingStudent-small.244b5340.jpg","../src/images/SmilingStudent-small.jpg"],"../src/images/SmilingStudent-small.jpg"],"./..\\images\\Header-1-grayscale.png":[["Header-1-grayscale.0d7a076e.png","../src/images/Header-1-grayscale.png"],"../src/images/Header-1-grayscale.png"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./..\\images\\SmilingStudent-small.jpg":[["SmilingStudent-small.244b5340.jpg","../src/images/SmilingStudent-small.jpg"],"../src/images/SmilingStudent-small.jpg"],"./..\\images\\studenthealthmodel.png":[["studenthealthmodel.5d760691.png","../src/images/studenthealthmodel.png"],"../src/images/studenthealthmodel.png"],"./..\\images\\Header-1-grayscale.png":[["Header-1-grayscale.0d7a076e.png","../src/images/Header-1-grayscale.png"],"../src/images/Header-1-grayscale.png"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -150,7 +150,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50204" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50102" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
